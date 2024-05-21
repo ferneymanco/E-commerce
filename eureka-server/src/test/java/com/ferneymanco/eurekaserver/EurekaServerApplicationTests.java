f0dc3954f118f64e1aac3aa5017a2e4a1985b2c8
LAPTOP-U2043B7F\ferney <ferney.manco@coordiutil.com>
2024-05-20 19:00:39 -0500

    Start project

---
+++ eureka-server/src/test/java/com/ferneymanco/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferneymanco.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
